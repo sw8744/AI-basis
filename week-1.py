3b48cf87e411241445d9ce82d06106b7439fb82a
thislife_won <sw8744@naver.com>
2023-09-05 09:42:50 +0900

    Week-1 Edited

--- week-1.py
+++ week-1.py
@@ -36,3 +36,53 @@ print(y_hat_inc)
 
 b_rate = (y_hat_inc - y_hat) / (b_inc - b)
 print(b_rate)
+
+b_new = b + 1
+print(b_new)
+
+err = y[0] - y_hat
+w_new = w + w_rate * err
+b_new = b + 1 * err
+print(w_new, b_new)
+
+y_hat = x[1] * w_new + b_new
+err = y[1] - y_hat
+w_rate = x[1]
+w_new = w_new + w_rate * err
+b_new = b_new + 1 * err
+print(w_new, b_new)
+
+for x_i, y_i in zip(x, y):
+    y_hat = x_i * w + b
+    err = y_i - y_hat
+    w_rate = x_i
+    w = w + w_rate * err
+    b = b + 1 * err
+print(w, b)
+
+# 가중치 조절 전
+plt.scatter(x, y)
+pt1 = (-0.1, -0.1 * 1.0 + 1.0) # w = b = 1.0
+pt2 = (0.15, 0.15 * 1.0 + 1.0) # w = 1.0, b = 1.0
+plt.plot([pt1[0], pt2[0]], [pt1[1], pt2[1]])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+# 가중치 조절 후
+plt.scatter(x, y)
+pt1 = (-0.1, -0.1 * w + b) # w = b = 1.0
+pt2 = (0.15, 0.15 * w + b) # w = b = 1.0
+plt.plot([pt1[0], pt2[0]], [pt1[1], pt2[1]])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+for i in range(1, 100):
+    for x_i, y_i in zip(x, y):
+        y_hat = x_i * w + b
+        err = y_i - y_hat
+        w_rate = x_i
+        w = w + w_rate * err
+        b = b + 1 * err
+print(w, b)
